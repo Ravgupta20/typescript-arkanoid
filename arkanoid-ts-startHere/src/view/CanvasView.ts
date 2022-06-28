371983917bf49206fe0190a639e95de8eb6cd2ef
Ravi Gupta <ravgupta20@gmail.com>
2022-06-27 23:56:45 -0400

    Added CanvasView class and constructor

--- arkanoid-ts-startHere/src/view/CanvasView.ts
+++ arkanoid-ts-startHere/src/view/CanvasView.ts
@@ -3,3 +3,23 @@ import { Brick } from "~/sprites/Brick";
 import { Paddle } from "~/sprites/Paddle";
 import { Ball } from "~/sprites/Ball";
 
+
+export class CanvasView {
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D | null;
+    private scoreDisplay: HTMLObjectElement | null;
+    private start: HTMLObjectElement | null;
+    private info: HTMLOListElement | null;
+
+    constructor(canvasName: string){
+        this.canvas = document.querySelector(canvasName) as HTMLCanvasElement;
+        this.context = this.canvas.getContext('2d');
+        this.scoreDisplay = document.querySelector('#score');
+        this.start = document.querySelector('#start');
+        this.info = document.querySelector('info');
+    }
+
+    clear(): void {
+        this.context?.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
