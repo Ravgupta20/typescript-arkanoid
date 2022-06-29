4d3f4810e56f06c0168f92bf6f95d23373734d8f
Ravi Gupta <ravgupta20@gmail.com>
2022-06-28 20:56:25 -0400

    Imported view, spirtes and images to index.ts

--- arkanoid-ts-startHere/src/index.ts
+++ arkanoid-ts-startHere/src/index.ts
@@ -1,2 +1,9 @@
-// Start here
-console.log("Start here!");
+import { CanvasView } from "./view/CanvasView";
+import { Ball } from "./sprites/Ball";
+import { Brick } from "./sprites/Brick";
+import { Paddle } from "./sprites/Paddle";
+
+//Images
+import PADDLE_IMAGE from './images/paddle.png';
+import BALL_IMAGE from './images/ball.png';
+
