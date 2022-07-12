42a47fbfb241b9e91f5a7db2029bb76c3417103d
Ravi Gupta <ravgupta20@gmail.com>
2022-07-11 21:48:40 -0400

    Wrote the startGame function

--- arkanoid-ts-startHere/src/index.ts
+++ arkanoid-ts-startHere/src/index.ts
@@ -19,6 +19,9 @@ import {
     BALL_STARTY
 } from './setup';
 
+//Helpers
+import { createBricks } from './helpers';
+
 let gameOver = false;
 let score = 0;
 
@@ -35,11 +38,26 @@ function setGameWin(view: CanvasView) {
 function gameLoop(
     view: CanvasView,
     bricks: Brick[],
-    paddle: Paddle,
-    ball: Ball
-) {}
+    // paddle: Paddle,
+    // ball: Ball
+) {
+    view.clear();
+    view.drawBricks(bricks);
 
-function startGame(view: CanvasView){}
+    requestAnimationFrame(() => gameLoop(view, bricks));
+}
+
+function startGame(view: CanvasView){
+    //Reset displays
+    score = 0;
+    view.drawInfo('');
+    view.drawScore(0);
+
+    //create all bricks
+    const bricks = createBricks();
+    
+    gameLoop(view, bricks);
+}
 
 const view  = new CanvasView('#playField');
 view.initStartButton(startGame);
